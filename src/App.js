6c8d94c9e7ef368fee0dbcab7511d2f5037afc0d
makeloff1 <makeloff2@gmail.com>
2019-11-09 16:02:50 +0900

    multiple-state

--- src/App.js
+++ src/App.js
@@ -1,50 +1,29 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
 
-  const increment = () => {
-    setCount(count + 1)
-  }
-  const decrement = () => {
-    setCount(count - 1)
-  }
-  const increment2 = () => {
-    setCount(previousCount => previousCount + 1)
-  }
-  const decrement2 = () => {
-    setCount(previousCount => previousCount - 1)
-  }
-  const reset = () => {
-    setCount(0)
-  }
-  const double = () => {
-    setCount(nextCount => nextCount * 2)
-  }
-  const three = () => {
-    setCount( (previousCount) => 
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    )
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+
+  const reset =() => {
+    setPrice(props.price)
+    setName(props.name)
   }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>Double</button>
-        <button onClick={three}>Three</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
