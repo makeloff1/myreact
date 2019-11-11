dbde775dadb317c239f1546a29e8fc7fd6cbcda2
makeloff1 <makeloff2@gmail.com>
2019-11-12 01:02:06 +0900

    solve prop drilling issue with Context

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import {
     CREATE_EVENT,
     DELETE_ALL_EVENTS
-}  from '../actions'
+} from '../actions'
 
-const EventForm = ({state, dispatch}) => {
+import AppContext from '../contexts/AppContext'
+
+const EventForm = () => {
+    const { state, dispatch } = useContext(AppContext)
     const [title, setTitle] = useState("")
     const [body, setBody] = useState("")
-  
+
     const addEvent = e => {
         e.preventDefault()
         dispatch({
